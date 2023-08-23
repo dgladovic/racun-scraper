66160be9900e55850dc36be1a95014c7d4c3e84e
Dimitrije Gladovic <rdomiran@gmail.com>
2023-08-24 01:03:21 +0200

    Registration: Firebase Integration

--- registrationController.js
+++ registrationController.js
@@ -1,59 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
-// Replace this with your actual secret key
-const secretKey = 'your-secret-key';
 
 router.use(bodyParser.json());
 router.use(cors({
-    origin: '*'
-  }));
+  origin: '*'
+}));
+
 // Route to register a new user
 router.post('/', async (req, res) => {
   try {
     console.log('registrate-app');
     let { email, password, first_name, last_name, date_of_birth } = req.body;
-    if(date_of_birth === ''){
+    if (date_of_birth === '') {
       date_of_birth = new Date();
     }
     const emailConv = email.toLowerCase();
-    // Check if the username is already taken
-    const client = await req.pool.connect();
-    const checkQuery = 'SELECT COUNT(*) FROM users WHERE email = $1';
-    const { rows } = await client.query(checkQuery, [emailConv]);
-    const existingCount = parseInt(rows[0].count, 10);
+    console.log(req.body);
 
-    if (existingCount > 0) {
-      client.release();
-      return res.status(400).json({ message: 'Email already reigstered!' });
+    const usersRef = req.admin.firestore().collection('users');
+    const userRecord = await usersRef.where('email', '==', emailConv).get();
+    console.log(userRecord);
+
+    if (!userRecord.empty) {
+      return res.status(400).json({ message: 'Email already registered!' });
     }
 
-    // Hash the password before storing it in the database
     bcrypt.hash(password, 10, async (err, hash) => {
       if (err) {
         return res.status(500).json({ message: 'Registration failed' });
       }
-
-      // Add the new user to the database
-      const insertQuery = `
-        INSERT INTO users (email, password, first_name, last_name, date_of_birth)
-        VALUES ($1, $2, $3, $4, $5)
-        RETURNING user_id
-      `;
-      
-      const result = await client.query(insertQuery, [emailConv, hash, first_name, last_name, date_of_birth]);
-      const newUser = { id: result.rows[0]['user_id'], email };
-
-      // Create and send a JWT token for the registered user
-    //   const token = jwt.sign({ userId: newUser.id }, secretKey, { expiresIn: '1h' });
-
-      client.release();
-      res.status(201).json({ 'success': `New user ${newUser.email} created!`, id: newUser.id });
+      const newUser = {
+        email: emailConv,
+        first_name: first_name,
+        last_name: last_name,
+        password: password,
+        date_of_birth: date_of_birth
+      };
+      const newUserRef = await usersRef.add(newUser);
+      res.status(201).json({ 'success': `New user ${newUserRef.email} created!`, id: newUserRef.id });
     });
   } catch (error) {
     console.error(error);
